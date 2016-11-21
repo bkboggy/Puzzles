bc69a59184767c4396d21e375c82fc974c461649
B.K <bkboggy@yahoo.com>
2016-11-20 17:40:19 -0800

    FizzBuzz solution in C#.

---
+++ CSharp/FizzBuzz.cs
@@ -0,0 +1,22 @@
+public class Solution 
+{
+    public IList<string> FizzBuzz(int n) 
+    {
+        var list = new List<string>();
+        for (int i = 1; i <= n; ++i)
+        {
+            string s = "";
+            if (i % 3 == 0)
+            {
+                s += "Fizz";
+            }
+            if (i % 5 == 0)
+            {
+                s += "Buzz";
+            }
+            
+            list.Add(s ?? i.ToString());
+        }
+        return list;
+    }
+}
